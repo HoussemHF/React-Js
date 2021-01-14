134928579cca16a35c3cc6d373f3c1cc09e337ed
Houssem Hafsaoui <infohoussem2701@gmail.com>
2021-01-14 10:00:33 +0100

    REACT JS

---
+++ login_form/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Components/Test/Test'
+import Form from './Components/Formulaire/Form';
+
+function App() {
+  return (
+
+    <div>
+
+<Test/>
+
+<Form/>
+    </div>
+
+
+
+  );
+}
+
+export default App;
